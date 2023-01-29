3b7bb823d84983a318da1dacd283a617aa9a112e
DexNex <111414781+DexNex@users.noreply.github.com>
2023-01-29 17:03:45 +0700

    Add files via upload

---
+++ Kpk dan Fpb.py
@@ -0,0 +1,11 @@
+judul = "pemecah soal kpk & fpb matematika".upper()
+judul_t = judul.center(200,"=")
+print(judul_t)
+
+print('''1.soal kpk
+2.soal fpb'''.upper())
+
+pilih1 = int(input("Masuk pilihan : "))
+_1 = 1 == True
+
+if pilih1 == 
